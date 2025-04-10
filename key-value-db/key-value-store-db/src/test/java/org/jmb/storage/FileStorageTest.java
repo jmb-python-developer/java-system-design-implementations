1e97f1650b5cf9140631dc8d9fc47129eb82f010
jmb-python-developer <165929314+jmb-python-developer@users.noreply.github.com>
2025-04-10 18:33:42 +0200

    Some changes

--- key-value-db/key-value-store-db/src/test/java/org/jmb/storage/FileStorageTest.java
+++ key-value-db/key-value-store-db/src/test/java/org/jmb/storage/FileStorageTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /**
  * Unit Test class for {@link FileStorage}
  */
@@ -14,25 +16,25 @@ public class FileStorageTest {
     private Storage<String, Map<String, String>> fileStorage;
 
     //Testing object without cache enabled
-    private Storage<String, Map<String, String>> fileStorageNoCache = new FileStorage<>(
-            "aTable", ".", true
-    );
+    private Storage<String, Map<String, String>> fileStorageNoCache;
 
     @BeforeEach
     void setUp() {
         fileStorage = new FileStorage<>(
-                "aTable", ".", true
+                "aTable", "key-value-db", false
         );
 
         fileStorageNoCache = new FileStorage<>(
-                "aTable", ".", true
+                "aTableCached", "key-value-db-cached", true
         );
     }
 
     @Test
     void testPutFirstEntry() {
         fileStorage.put("key1", Map.of("Name", "Juan Bruno"));
+        fileStorage.put("key2", Map.of("Name", "Carlos Bruno"));
         fileStorageNoCache.put("key1", Map.of("Name", "Juan Bruno"));
-
+        var savedValue = fileStorage.get("key1");
+        assertEquals(savedValue.get(), Map.of("Name", "Juan Bruno"));
     }
 }
